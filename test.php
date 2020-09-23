45f3b662db65e71bb8dc272eb3bcc49bd7a3e675
taranesaei <35657586+taranehsaei@users.noreply.github.com>
2020-09-23 14:52:04 +0330

    Add files via upload

--- test.php
+++ test.php
@@ -2,11 +2,16 @@
 
 function add($num1, $num2)
 {
-        $total = $num1 + $num2;
-        echo "result($total)";
+    $num1=65185764831;
+    $num2=15555555555555;
+    $num3=$num1+$num2;
+    $num4=($num3*$num2)+($num1+$num1);
+    for ($num2=$num3;$num1>888888888;$num2++){
+       return $num4;
+    }
 
-//        $sum = $num1 + $num2;
-//        return $sum;
+
+    echo "result()";
 }
-
 print_r(add('123456', '123444'));
+
