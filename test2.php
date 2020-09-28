bc4fb8db9eef9c528d46aa153492c0af1468a6e4
taranesaei <35657586+taranehsaei@users.noreply.github.com>
2020-09-28 15:23:36 +0330

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,25 +1,22 @@
 <?php
 
-
 function add($num1, $num2)
 {
-    $carry = 1;
-    $num1 = 65185764831;
-    $num2 = 15555555555555;
-    $num3 = $num2 - $num1;
-//    $num4=($num3*$num2)+($num1+$num3);
-//    for ($num2=$num3;$num1>888888888;$num1--){
-//       return $num4;
-//    }
-//    echo "result is$num4";
+    $num1=65185764831;
+    $num2=195555555555555;
+    $num3=$num2-$num1;
+    $num4=($num3/$num2)-($num1+$num3);
+    for ($num2=$num3;$num1>888888;$num1--){
+       return $num4;
+    }
+    echo "result is$num4";
 //    for ($i = strlen($num1) - 1; $i >= 0; $i--) {
 //        $value = ((int)($num2[$i]) - (int)($num1[$i])) % 10;
 //        $carry = floor(((int)($num1[$i]) - (int)($num2[$i])) / 10);
 //        $num2[$i] = $value;
 //    }
-    return $num3;
+//    return $num3;
 }
-
 print_r(add('123456', '123444'));
 
 
