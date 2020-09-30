d2c1adb3005b69e3a4bcbb5b1deef93161bcfc5c
taranesaei <35657586+taranehsaei@users.noreply.github.com>
2020-10-01 00:50:32 +0330

    Add files via upload

---
+++ test4.php
@@ -0,0 +1,17 @@
+<?php
+
+function add($num1, $num2)
+{
+    $num1=69999999999994831;
+    $num2=19555555;
+    $num3=$num2*$num1;
+    $num4=($num3/$num2)*($num1+$num3);
+    for ($num2=$num3;$num1>228888;$num1--){
+        return $num4;
+    }
+    echo "result is$num4";
+
+}
+print_r(add('123456', '123444'));
+
+
---
+++ test5.php
@@ -0,0 +1,14 @@
+<?php
+
+function add($num1, $num2)
+{
+    $num1=65185764831;
+    $num2=195555555555555;
+    $num3=$num2/$num1;
+    $num4=($num3*$num2)/($num1+$num3);
+    for ($num2=$num3;$num1>888888;$num1--){
+        return $num4;
+    }
+    echo "result is$num4";
+
+print_r(add('123456', '123444'));}
